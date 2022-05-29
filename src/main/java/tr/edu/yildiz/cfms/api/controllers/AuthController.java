51bf2b40374103dea1880e2812bdf1fc5b3ccf89
abarisyerlikaya <abarisyerlikaya@gmail.com>
2022-05-29 18:31:29 +0300

    Bug fix

--- src/main/java/tr/edu/yildiz/cfms/api/controllers/AuthController.java
+++ src/main/java/tr/edu/yildiz/cfms/api/controllers/AuthController.java
@@ -1,22 +1,22 @@
-//package tr.edu.yildiz.cfms.api.controllers;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import tr.edu.yildiz.cfms.api.models.LoginRequest;
-//import tr.edu.yildiz.cfms.business.abstracts.AuthService;
-//import tr.edu.yildiz.cfms.core.response_types.Response;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class AuthController {
-//    @Autowired
-//    private AuthService authService;
-//
-//    @PostMapping("/login")
-//    public Response login(@RequestBody LoginRequest request) {
-//        return authService.login(request);
-//    }
-//}
+package tr.edu.yildiz.cfms.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tr.edu.yildiz.cfms.api.models.LoginRequest;
+import tr.edu.yildiz.cfms.business.abstracts.AuthService;
+import tr.edu.yildiz.cfms.core.response_types.Response;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public Response login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
