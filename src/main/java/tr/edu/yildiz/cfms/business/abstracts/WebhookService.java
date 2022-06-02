5d4364f9d7133cb80641c377a89cf70864e27c12
abarisyerlikaya <abarisyerlikaya@gmail.com>
2022-06-02 10:18:31 +0300

    Twitter completed

--- src/main/java/tr/edu/yildiz/cfms/business/abstracts/WebhookService.java
+++ src/main/java/tr/edu/yildiz/cfms/business/abstracts/WebhookService.java
@@ -4,10 +4,15 @@ import org.springframework.stereotype.Service;
 import tr.edu.yildiz.cfms.api.dtos.webhooks.facebook.FacebookWebhookDto;
 import tr.edu.yildiz.cfms.api.dtos.webhooks.instagram.InstagramConversationDto;
 import tr.edu.yildiz.cfms.api.dtos.webhooks.telegram.TelegramWebhookDto;
+import tr.edu.yildiz.cfms.api.dtos.webhooks.twitter.TwitterWebhookDto;
+
+import java.util.Map;
 
 @Service
 public interface WebhookService {
     void handleFacebookWebhook(FacebookWebhookDto dto);
     void handleTelegramWebhook(TelegramWebhookDto dto);
     void handleInstagramConversation(InstagramConversationDto dto, Boolean isNew);
+    Map<String, String> verifyTwitterWebhook(String crcToken);
+    void handleTwitterWebhook(TwitterWebhookDto dto);
 }
--- src/main/java/tr/edu/yildiz/cfms/core/utils/Constants.java
+++ src/main/java/tr/edu/yildiz/cfms/core/utils/Constants.java
@@ -7,4 +7,8 @@ public class Constants {
     public static final String FB_PAGE_ID = "102412562397246";
     public static final String TELEGRAM_BASE_URL = "https://api.telegram.org";
     public static final String TELEGRAM_TOKEN = "5399006786:AAGmP5GLg7_ZPOaujPBWoAm8ObiiQhY1wjA";
+    public static final String CONSUMER_SECRET = "jLy2vTyXS1UEUZ1QsDMDdxpa4GLiKT0MAISmqrBDeOSxm4tvjt";
+    public static final String TWITTER_MESSAGE_URL = "https://api.twitter.com/1.1/direct_messages/events/new.json";
+    public static final String TWITTER_ACCOUNT_ID = "1250827210669375491";
+    public static final String TWITTER_AUTH_HEADER = "OAuth oauth_consumer_key=\"IpG3iHNtmfrH2CGF0Ox0Z2yuY\",oauth_token=\"1250827210669375491-9TVHcjWva789N16a8PyFm4pD87uOIP\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1654107141\",oauth_nonce=\"Vo9lRvcce73\",oauth_version=\"1.0\",oauth_signature=\"Y1oJ53wuZDxQ4eJnLcEFmkQ7Aeg%3D\"";
 }
