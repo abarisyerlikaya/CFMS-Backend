b4218118438685959094a3dbca29b356f5f21927
abarisyerlikaya <abarisyerlikaya@gmail.com>
2024-08-12 17:00:34 +0300

    Added constants without secrets

---
+++ src/main/java/tr/edu/yildiz/cfms/core/utils/Constants.java
@@ -0,0 +1,18 @@
+package tr.edu.yildiz.cfms.core.utils;
+
+public class Constants {
+    public static final String FB_BASE_URL = "https://graph.facebook.com";
+    public static final String FB_PAGE_ACCESS_TOKEN = "{FB_PAGE_ACCESS_TOKEN}";
+    public static final String FB_VERIFY_TOKEN = "{FB_VERIFY_TOKEN}";
+    public static final String TELEGRAM_BASE_URL = "https://api.telegram.org";
+    public static final String TELEGRAM_TOKEN = "{TELEGRAM_TOKEN}";
+    public static final String CONSUMER_SECRET = "{CONSUMER_SECRET}";
+    public static final String TWITTER_MESSAGE_URL = "https://api.twitter.com/1.1/direct_messages/events/new.json";
+    public static final String TWITTER_ACCOUNT_ID = "{TWITTER_ACCOUNT_ID}";
+    public static final String TWITTER_AUTH_HEADER = "{TWITTER_AUTH_HEADER}";
+    public static final String ADMIN_JWT = "{ADMIN_JWT}";
+    public static final String IG_USERNAME = "{IG_USERNAME}";
+    public static final String IG_PASSWORD = "{IG_PASSWORD}";
+    public static final String BEARER = "Bearer ";
+    public static final int BEARER_LENGTH = BEARER.length();
+}
